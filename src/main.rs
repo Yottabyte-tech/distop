e7f0f2620dd54573ddd339f3f0d67032c546e0c2
Callan McClish <callanchem314@gmail.com>
2026-06-08 08:44:04 -0700

    New Design added

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,9 @@ pub struct RAMGraph {
     pub graph_data: String,
 }
 
+
+pub static NODE_INDEX: LazyLock<Mutex<i32>> = LazyLock::new(|| Mutex::new(0));
+
 pub static CORE_GRAPHS: LazyLock<Mutex<Vec<CoreGraph>>> = LazyLock::new(|| Mutex::new(Vec::new()));
 pub static RAM_GRAPHS: LazyLock<Mutex<Vec<RAMGraph>>> = LazyLock::new(|| Mutex::new(Vec::new()));
 
@@ -107,11 +110,26 @@ async fn render_tui() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     loop {
         terminal.draw(render_app)?;
-
         if event::poll(std::time::Duration::from_millis(1000))? {
             if let Event::Key(key) = event::read()? {
                 if key.code == KeyCode::Char('q') {
                     break;
+                }
+                if key.code == KeyCode::Up {
+                    let mut index = NODE_INDEX.lock().unwrap();
+                    if *index == 0 {
+                        *index = NODE_LIST.lock().unwrap().len() as i32 - 1;
+                    } else {
+                        *index -= 1;
+                    }
+                }
+                if key.code == KeyCode::Down {
+                    let mut index = NODE_INDEX.lock().unwrap();
+                    if *index == NODE_LIST.lock().unwrap().len() as i32 - 1{
+                        *index = 0;
+                    } else {
+                        *index += 1;
+                    }
                 }
             }
         }
@@ -126,7 +144,22 @@ fn render_app(frame: &mut Frame) {
 
     // Import the data list
     let list = NODE_LIST.lock().unwrap();
-        if list.len() > 0 {
+    if list.len() > 0 {
+
+        let mut name_select = Text::default();
+
+        let node_index: i32 = *NODE_INDEX.lock().unwrap() as i32;
+        
+        for (index, node) in list.iter().enumerate(){
+             
+            let hostname: &String = &node.info.hostname;
+
+            if node_index == index as i32{
+                name_select.lines.push(Line::from(format!("{}", hostname)).black().on_white());
+            } else {
+                name_select.lines.push(Line::from(format!("{}", hostname)));
+            }
+        }
         
         let graph_chars: Vec<String> = vec![
             "⣀".to_string(),
@@ -140,102 +173,62 @@ fn render_app(frame: &mut Frame) {
 
         let horizontal = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints([Constraint::Min(52), Constraint::Min(42), Constraint::Percentage(100)])
+            .constraints([Constraint::Min(32), Constraint::Min(52), Constraint::Min(42), Constraint::Percentage(100)])
             .split(frame.area());
 
 
         let vertical = Layout::default()
             .direction(Direction::Vertical)
             .constraints([Constraint::Percentage(40), Constraint::Percentage(60)])
-            .split(horizontal[2]);
+            .split(horizontal[3]);
 
+        
         let mut computer_usage = Text::default();
         
         let mut ram_usage = Text::default();
 
         let mut network_usage = Text::default();
 
+        
 
-        for list_elem in list.iter(){
-            let node_cpu_usage = format!("CPU: [ {:.2}% ]", list_elem.info.cpu_usage);
+        let list_elem = &list[node_index as usize];
+
+
+
+
+
+
+        let node_cpu_usage = format!("CPU: [ {:.2}% ]", list_elem.info.cpu_usage);
             
-            // Push each line explicitly to allow granular styling
-            computer_usage.lines.push(Line::from("╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌").fg(Color::Rgb(100,100,100))); // Second newline
-            computer_usage.lines.push(Line::from(vec![
-                Span::raw("╭─┤ "),
-                Span::raw(list_elem.info.hostname.to_string()).bold().underlined(),
-                Span::raw(" │"),
-            ]));
-            computer_usage.lines.push(Line::from("│"));
-            computer_usage.lines.push(Line::from(format!("├─┤ {} {}", node_cpu_usage, list_elem.info.cpu_temp)));
-            computer_usage.lines.push(Line::from("│"));
+        // Push each line explicitly to allow granular styling
+        computer_usage.lines.push(Line::from("╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌").fg(Color::Rgb(100,100,100))); // Second newline
+        computer_usage.lines.push(Line::from(vec![
+            Span::raw("╭─┤ "),
+            Span::raw(list_elem.info.hostname.to_string()).bold().underlined(),
+            Span::raw(" │"),
+        ]));
+        computer_usage.lines.push(Line::from("│"));
+        computer_usage.lines.push(Line::from(format!("├─┤ {} {}", node_cpu_usage, list_elem.info.cpu_temp)));
+        computer_usage.lines.push(Line::from("│"));
  
 
-            for (_index, core) in list_elem.info.cores.iter().enumerate(){
+        for (_index, core) in list_elem.info.cores.iter().enumerate(){
                 
-                let core_index: i32 = _index as i32;
+            let core_index: i32 = _index as i32;
                 
-                let mut core_list = CORE_GRAPHS.lock().unwrap();
+            let mut core_list = CORE_GRAPHS.lock().unwrap();
                 
-                let core_graph_data = core_list.iter().position(|item| item.core_index == core_index && item.hostname == list_elem.info.hostname.to_string());
+            let core_graph_data = core_list.iter().position(|item| item.core_index == core_index && item.hostname == list_elem.info.hostname.to_string());
                 
-                let map_to_bar: f32 = (core/100.0) * 4.0;
-
-                let mut graph: String = graph_chars[map_to_bar.round().max(1.0) as usize - 1].clone();
-                
-                match core_graph_data {
-                    Some(_index) => {
-
-                        // Get graphing struct
-                        let item = &core_list[_index];
-                        
-                        let mut new_graph = format!("{}{}", item.graph_data.clone(), graph);
-                        
-                        if new_graph.chars().count() > 20 {
-                            if !new_graph.is_empty() {
-                                new_graph.remove(0); 
-                            }
-                        }
-
-                        core_list[_index].graph_data = new_graph.clone();
-                        
-                        graph = new_graph;
-                    }
-                    None => core_list.push(CoreGraph { hostname: list_elem.info.hostname.to_string(), core_index: core_index, graph_data: "                    ".to_string()}),
-                }
-
-                // Graph spacing
-                let spacing: usize = 10 - format!("{}{:.2}%", core_index, core).len().min(10);
-                
-                let core_u8: u8 = *core as u8;
-                
-                let mut connecting_char: String = "├".to_string();
-
-                if core_index + 1 as i32 == list_elem.info.cores.len() as i32{
-                    connecting_char = "╰".to_string();
-                }
-
-                let core_line = Line::from(vec![
-                    Span::raw(format!("{}─┤ Core {}: {:.2}%{}[ ", connecting_char, core_index, core, " ".to_string().repeat(spacing))),
-                    Span::raw(format!("{}", graph)).bold().fg(Color::Rgb(2 * core_u8, 2 * (101 - core_u8), 0)),
-                    Span::raw(" ]")
-                ]);
-                computer_usage.lines.push(core_line);
-            }
-
-
-            let mut ram_list = RAM_GRAPHS.lock().unwrap();
-            
-            let ram_graph_data = ram_list.iter().position(|item| item.hostname == list_elem.info.hostname.to_string());
-
-            let map_to_bar: f64 = (list_elem.info.ram_used_gb/list_elem.info.ram_total_gb) * 4.0;
+            let map_to_bar: f32 = (core/100.0) * 4.0;
 
             let mut graph: String = graph_chars[map_to_bar.round().max(1.0) as usize - 1].clone();
-            match ram_graph_data {
+                
+            match core_graph_data {
                 Some(_index) => {
 
                     // Get graphing struct
-                    let item = &ram_list[_index];
+                    let item = &core_list[_index];
                         
                     let mut new_graph = format!("{}{}", item.graph_data.clone(), graph);
                         
@@ -245,45 +238,103 @@ fn render_app(frame: &mut Frame) {
                         }
                     }
 
-                    ram_list[_index].graph_data = new_graph.clone();
+                    core_list[_index].graph_data = new_graph.clone();
                         
                     graph = new_graph;
                 }
-                None => ram_list.push(RAMGraph { hostname: list_elem.info.hostname.to_string(), graph_data: "                    ".to_string()}),
+                None => core_list.push(CoreGraph { hostname: list_elem.info.hostname.to_string(), core_index: core_index, graph_data: "                    ".to_string()}),
             }
+
+            // Graph spacing
+            let spacing: usize = 10 - format!("{}{:.2}%", core_index, core).len().min(10);
+                
+            let core_u8: u8 = *core as u8;
+                
+            let mut connecting_char: String = "├".to_string();
+
+            if core_index + 1 as i32 == list_elem.info.cores.len() as i32{
+                connecting_char = "╰".to_string();
+            }
+
+            let core_line = Line::from(vec![
+                Span::raw(format!("{}─┤ Core {}: {:.2}%{}[ ", connecting_char, core_index, core, " ".to_string().repeat(spacing))),
+                Span::raw(format!("{}", graph)).bold().fg(Color::Rgb(2 * core_u8, 2 * (101 - core_u8), 0)),
+                Span::raw(" ]")
+            ]);
+            computer_usage.lines.push(core_line);
+        }
+
+
+        let mut ram_list = RAM_GRAPHS.lock().unwrap();
+            
+        let ram_graph_data = ram_list.iter().position(|item| item.hostname == list_elem.info.hostname.to_string());
+
+        let map_to_bar: f64 = (list_elem.info.ram_used_gb/list_elem.info.ram_total_gb) * 4.0;
+
+        let mut graph: String = graph_chars[map_to_bar.round().max(1.0) as usize - 1].clone();
+        match ram_graph_data {
+            Some(_index) => {
+
+                // Get graphing struct
+                let item = &ram_list[_index];
+                        
+                let mut new_graph = format!("{}{}", item.graph_data.clone(), graph);
+                        
+                if new_graph.chars().count() > 20 {
+                    if !new_graph.is_empty() {
+                        new_graph.remove(0); 
+                    }
+                }
+
+                ram_list[_index].graph_data = new_graph.clone();
+                        
+                graph = new_graph;
+            }
+            None => ram_list.push(RAMGraph { hostname: list_elem.info.hostname.to_string(), graph_data: "                    ".to_string()}),
+        }
            
 
-            let ram_u8: u8 = (100.0 * list_elem.info.ram_used_gb/list_elem.info.ram_total_gb) as u8;
+        let ram_u8: u8 = (100.0 * list_elem.info.ram_used_gb/list_elem.info.ram_total_gb) as u8;
 
-            ram_usage.lines.push(Line::from("╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌").fg(Color::Rgb(100,100,100))); // Second newline
-            ram_usage.lines.push(Line::from(vec![
-                Span::raw("╭─┤ "),
-                Span::raw(list_elem.info.hostname.clone()).underlined(),
-                Span::raw(" │"),
-            ]));
-            ram_usage.lines.push(Line::from("│"));
-            ram_usage.lines.push(Line::from(format!("├─┤ RAM: {:.2}% ({:.2}GB/{:.2}GB)", 100.0 * list_elem.info.ram_used_gb / list_elem.info.ram_total_gb ,list_elem.info.ram_used_gb, list_elem.info.ram_total_gb)));
-            ram_usage.lines.push(Line::from("│"));
+        ram_usage.lines.push(Line::from("╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌").fg(Color::Rgb(100,100,100))); // Second newline
+        ram_usage.lines.push(Line::from(vec![
+            Span::raw("╭─┤ "),
+            Span::raw(list_elem.info.hostname.clone()).underlined(),
+            Span::raw(" │"),
+        ]));
+        ram_usage.lines.push(Line::from("│"));
+        ram_usage.lines.push(Line::from(format!("├─┤ RAM: {:.2}% ({:.2}GB/{:.2}GB)", 100.0 * list_elem.info.ram_used_gb / list_elem.info.ram_total_gb ,list_elem.info.ram_used_gb, list_elem.info.ram_total_gb)));
+        ram_usage.lines.push(Line::from("│"));
 
-            ram_usage.lines.push(Line::from(vec![
-                Span::raw("╰─┤ [ "),
-                Span::raw(graph).bold().fg(Color::Rgb(2 * ram_u8, 2 * (101 - ram_u8), 0)),
-                Span::raw(" ]"),
-                Span::raw(list_elem.info.network[0].interface.clone()),
-            ]));
+        ram_usage.lines.push(Line::from(vec![
+            Span::raw("╰─┤ [ "),
+            Span::raw(graph).bold().fg(Color::Rgb(2 * ram_u8, 2 * (101 - ram_u8), 0)),
+            Span::raw(" ]"),
+            Span::raw(list_elem.info.network[0].interface.clone()),
+        ]));
 
             
-            network_usage.lines.push(Line::from("╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌").fg(Color::Rgb(100,100,100))); // Second newline
-            network_usage.lines.push(Line::from(vec![
-                Span::raw("╭─┤ "),
-                Span::raw(list_elem.info.hostname.clone()).underlined(),
-                Span::raw(" │"),
-            ]));
-            network_usage.lines.push(Line::from("│"));
+        network_usage.lines.push(Line::from("╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌").fg(Color::Rgb(100,100,100))); // Second newline
+        network_usage.lines.push(Line::from(vec![
+            Span::raw("╭─┤ "),
+            Span::raw(list_elem.info.hostname.clone()).underlined(),
+            Span::raw(" │"),
+        ]));
+        network_usage.lines.push(Line::from("│"));
 
-        }
+    
         
 
+        let name_select_block = Paragraph::new(name_select).fg(Color::White)
+            .block(Block::default()
+                   .borders(Borders::ALL)
+                   .title(Line::from(vec![
+                        Span::raw("╯Nodes╰"),
+                   ]))
+                   .border_type(BorderType::Rounded)
+                   .fg(Color::Rgb(150,75,75))
+                   .bg(Color::Rgb(0,0,0))
+            );
 
 
         // Create the first paragraph block
@@ -291,15 +342,10 @@ fn render_app(frame: &mut Frame) {
             .block(Block::default()
                    .borders(Borders::ALL)
                    .title(Line::from(vec![
-                        Span::raw("╯"),
-                        Span::raw("C").red().underlined().bold(),
-                        Span::raw("PU").white(),
-                        Span::raw("╰───╯"),
-                        Span::raw("Search:_______").white(),
-                        Span::raw("╰"),
+                        Span::raw("╯CPU╰"),
                    ]))
                    .border_type(BorderType::Rounded)
-                   .fg(Color::Rgb(150,75,75))
+                   .fg(Color::Rgb(150,150,75))
                    .bg(Color::Rgb(0,0,0))
             );
             
@@ -308,12 +354,7 @@ fn render_app(frame: &mut Frame) {
             .block(Block::default()
                    .borders(Borders::ALL)
                    .title(Line::from(vec![
-                        Span::raw("╯"),
-                        Span::raw("R").red().underlined(),
-                        Span::raw("AM").white(),
-                        Span::raw("╰───╯"),
-                        Span::raw("Search:_______").white(),
-                        Span::raw("╰"),
+                        Span::raw("╯RAM╰"),
                    ]))
                    .border_type(BorderType::Rounded)
                    .fg(Color::Rgb(75,150,75))
@@ -323,15 +364,16 @@ fn render_app(frame: &mut Frame) {
         let network_block = Paragraph::new(network_usage).fg(Color::White)
             .block(Block::default()
                    .borders(Borders::ALL)
-                   .title("╯Network╰")
+                   .title("╯Nodes╰").white()
                    .border_type(BorderType::Rounded)
                    .fg(Color::Rgb(75,75,150))
                    .bg(Color::Rgb(0,0,0))
             );
 
 
-        frame.render_widget(cpu_block, horizontal[0]);
-        frame.render_widget(ram_block, horizontal[1]);
+        frame.render_widget(name_select_block, horizontal[0]);
+        frame.render_widget(cpu_block, horizontal[1]);
+        frame.render_widget(ram_block, horizontal[2]);
         frame.render_widget(network_block, vertical[0]);
     }
 }
